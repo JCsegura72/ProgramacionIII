0694a0b1f62863e18f38737ebb3996c2ed35eda7
JCsegura72 <seguracamilo721@gmail.com>
2026-04-10 06:37:48 -0500

    Segundo corte

---
+++ Segundo Corte/EjerciciosMaquetación/src/Ejercicio 1 - Formulario de Perfil.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+public class FormPerfil : Form
+{
+    public FormPerfil()
+    {
+        Text = "Mi Cuenta";
+        Size = new Size(400, 350);
+
+        GroupBox gb = new GroupBox()
+        {
+            Text = "Información Personal",
+            Dock = DockStyle.Fill
+        };
+
+        Label lblNombre = new Label() { Text = "Nombre:", Location = new Point(20, 30) };
+        TextBox txtNombre = new TextBox() { Location = new Point(150, 30), Width = 200 };
+
+        Label lblApellido = new Label() { Text = "Apellido:", Location = new Point(20, 70) };
+        TextBox txtApellido = new TextBox() { Location = new Point(150, 70), Width = 200 };
+
+        Label lblCorreo = new Label() { Text = "Correo:", Location = new Point(20, 110) };
+        TextBox txtCorreo = new TextBox() { Location = new Point(150, 110), Width = 200 };
+
+        Label lblTelefono = new Label() { Text = "Teléfono:", Location = new Point(20, 150) };
+        MaskedTextBox txtTelefono = new MaskedTextBox("(999) 000-0000")
+        {
+            Location = new Point(150, 150),
+            Width = 200
+        };
+
+        GroupBox gbGenero = new GroupBox()
+        {
+            Text = "Género",
+            Location = new Point(20, 190),
+            Size = new Size(330, 50)
+        };
+
+        RadioButton rb1 = new RadioButton() { Text = "Masculino", Location = new Point(10, 20) };
+        RadioButton rb2 = new RadioButton() { Text = "Femenino", Location = new Point(110, 20) };
+        RadioButton rb3 = new RadioButton() { Text = "Otro", Location = new Point(210, 20) };
+
+        gbGenero.Controls.AddRange(new Control[] { rb1, rb2, rb3 });
+
+        Button btnGuardar = new Button()
+        {
+            Text = "Guardar Cambios",
+            Location = new Point(120, 260),
+            Width = 150
+        };
+
+        gb.Controls.AddRange(new Control[] {
+            lblNombre, txtNombre,
+            lblApellido, txtApellido,
+            lblCorreo, txtCorreo,
+            lblTelefono, txtTelefono,
+            gbGenero, btnGuardar
+        });
+
+        Controls.Add(gb);
+    }
+
+    [STAThread]
+    static void Main()
+    {
+        Application.Run(new FormPerfil());
+    }
+}
---
+++ Segundo Corte/EjerciciosMaquetación/src/Ejercicio 2 - Panel con Pestañas.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+public class FormTabs : Form
+{
+    public FormTabs()
+    {
+        Text = "Configuración";
+        Size = new Size(400, 300);
+
+        TabControl tabs = new TabControl() { Dock = DockStyle.Fill };
+
+        // Apariencia
+        TabPage tab1 = new TabPage("Apariencia");
+
+        Label lblTema = new Label() { Text = "Tema del Sistema", Location = new Point(20, 20) };
+        ComboBox combo = new ComboBox()
+        {
+            Location = new Point(20, 50),
+            Width = 150
+        };
+        combo.Items.AddRange(new string[] { "Claro", "Oscuro", "Azul Cobalto" });
+
+        CheckBox chkAnim = new CheckBox()
+        {
+            Text = "Activar animaciones de ventana",
+            Location = new Point(20, 90)
+        };
+
+        tab1.Controls.AddRange(new Control[] { lblTema, combo, chkAnim });
+
+        // Notificaciones
+        TabPage tab2 = new TabPage("Notificaciones");
+
+        CheckBox c1 = new CheckBox() { Text = "Recibir correos", Location = new Point(20, 20) };
+        CheckBox c2 = new CheckBox() { Text = "Alertas de escritorio", Location = new Point(20, 50) };
+        CheckBox c3 = new CheckBox() { Text = "Sonidos de sistema", Location = new Point(20, 80) };
+
+        NumericUpDown num = new NumericUpDown()
+        {
+            Location = new Point(20, 120),
+            Minimum = 0,
+            Maximum = 100
+        };
+
+        tab2.Controls.AddRange(new Control[] { c1, c2, c3, num });
+
+        tabs.TabPages.Add(tab1);
+        tabs.TabPages.Add(tab2);
+
+        Controls.Add(tabs);
+    }
+
+    [STAThread]
+    static void Main()
+    {
+        Application.Run(new FormTabs());
+    }
+}
